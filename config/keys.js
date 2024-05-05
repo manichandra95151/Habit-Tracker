2ef4c3bc65531836030151b77148f79c7fbb53a2
Mani Chandra <82253140+manichandra95151@users.noreply.github.com>
2024-05-05 17:03:19 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-     MongoURI: 'mongodb://127.0.0.1:27017/habits_db'    
+     MongoURI: 'mongodb+srv://manichandra9151:kYp9QrH2mJ2zVpZt@cluster0.1vkphlg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'    
 }
